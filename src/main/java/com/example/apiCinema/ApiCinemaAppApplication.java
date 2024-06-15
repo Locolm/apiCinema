a08a9d846016a8fa29a49711c43e64a4bac1262c
Locolm <clement.hl34@gmail.com>
2024-06-15 13:33:36 +0200

    first commit

---
+++ src/main/java/com/example/apiCinema/ApiCinemaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCinemaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCinemaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiCinema/ApiCinemaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCinemaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
